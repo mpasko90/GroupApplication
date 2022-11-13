2f887266fc1cb5dfebde8d0116b3949aa96cfc82
mpasko90 <91828918+mpasko90@users.noreply.github.com>
2022-11-13 14:53:33 +0100

    Gotowe 3 pierwsze widoki + przejście do menu

--- app/src/main/java/com/mpasko/groupapplication/ThirdActivity.kt
+++ app/src/main/java/com/mpasko/groupapplication/ThirdActivity.kt
@@ -1,11 +1,19 @@
 package com.mpasko.groupapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
+
+        val buttonClick = findViewById<ConstraintLayout>(R.id.nextBtn1)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
